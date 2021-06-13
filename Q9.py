436cf45096b91b2282038ace448a0df64ece8631
AviyaDavid <73593533+AviyaDavid@users.noreply.github.com>
2021-06-13 16:17:16 +0300

    Q9 with issues!

--- Q9.py
+++ Q9.py
@@ -1,190 +1,75 @@
-from datetime import *
-from decimal import getcontext, Decimal
+from math import e, cos, sin
+import numpy as np
 
 
-import math
-import sympy as sy
-from sympy import exp
-from sympy.utilities.lambdify import lambdify
-
-x = sy.symbols('x')
-
-def printRoots(rootsList):
-    if None in rootsList:
-        return
-    for i in range(len(rootsList)):
-        print("Root: {0}    Iterations: {1}\n".format(rootsList[i][0], rootsList[i][1]))
+def f(x):
+    # for calculation of function in point x
+    return (sin(x ** 4 + 5 * x - 6)) / (2 * e(-2 * x + 5))
 
 
-def Bisection_Method(polynomial, start_point, end_point, epsilon):
-    check = findSuspects(polynomial, start_point, end_point)
-    roots = []
-
-    for i in range(len(check)):  # for every suspected section found
-        a = check[i][0]
-        b = check[i][1]
-        roots.append(subBisection(a, b, polynomial, epsilon))
-
-    p_prime = polynomial.diff(x)
-    check = findSuspects(p_prime, start_point, end_point)
-    for i in range(len(check)):  # for every suspected section found
-        a = check[i][0]
-        b = check[i][1]
-        tup = subBisection(a, b, polynomial, epsilon)
-        res = polynomial.subs(x, tup[0])
-        if res + epsilon >= 0 and res - epsilon <= 0:
-            print("Root: {0}    Iterations: {1}\n".format(0, tup[1]))
-        else:
-            continue
-    for i in range(len(roots) - 1):
-        if roots[i][0] + epsilon >= 0 and roots[i][0] - epsilon <= 0:
-            del roots[i]
-
-    printRoots(roots)
+def fTag(x):
+    # for calculation of function prime in point x
+    return (5 / 2) * (e(2 * x - 5) * cos((x ** 4) - 6 + 5 * x) + 2 * e(2 * x - 5) * (x ** 3) * cos(
+        (x ** 4) - 6 + 5 * x) + math.e(2 * x))
 
 
-def subBisection(a, b, p, eps):
-    maxIter = int(math.ceil((-1) * (math.log(eps / (b - a)) / math.log(2))))
-    iter = 0
-    while abs(b - a) > eps:
-        if maxIter >= iter:
-            iter += 1
-            c = (a + b) / 2
-            fa = p.subs(x, a)
-            fb = p.subs(x, b)
-            fc = p.subs(x, c)
-            if fa * fc > 0:  # ignore left section
-                a = c
-            else:
-                b = c
-        else:
-            print("Cannot solve by bisection method!\n")
-            return
-    return (c, iter)
+def newton(x0, x1, eps):
+    # Newton Raphson Method
+    min = x0
+    max = x1
+    xi = x0
+    iter = 1
+    xiList = []
+    fxList = []
+    fTagxList = []
 
-
-def Newton_Raphson(polynomial, start_point, end_point, epsilon):
-    check = findSuspects(polynomial, start_point, end_point)
-    roots = []
-    p_prime = polynomial.diff(x)
-    for i in range(len(check)):  # for every suspected section found
-        a = check[i][0]
-        b = check[i][1]
-        roots.append(subNewtonRaphson(a, b, polynomial, epsilon, p_prime))
-
-    p_sec_prime = p_prime.diff(x)
-    check = findSuspects(p_prime, start_point, end_point, )
-    if None not in check:
-        for i in range(len(check)):  # for every suspected section found
-            a = check[i][0]
-            b = check[i][1]
-            tup = subNewtonRaphson(a, b, polynomial, epsilon, p_sec_prime)
-            res = polynomial.subs(x, tup[0])
-            if res + epsilon >= 0 and res - epsilon <= 0:
-                roots.append(tup)
-            else:
-                continue
-
-        for i in range(len(roots) - 1):
-            if roots[i][0] + epsilon >= 0 and roots[i][0] - epsilon <= 0:
-                del roots[i]
-
-    printRoots(roots)
-
-
-def subNewtonRaphson(a, b, p, eps, p_prime):
-    maxIter = int(math.ceil((-1) * (math.log(eps / (b - a)) / math.log(2))))
-    iter = 0
-    c = (a + b) / 2
     while True:
-        if maxIter >= iter:
-            iter += 1
-            fc = p.subs(x, c)
-            fTagC = p_prime.subs(x, c)
-            prevC = c
-            c = prevC - (fc / fTagC)
-            if fc == 0 or abs(c - prevC) <= eps:
-                return (c, iter)
-            else:
-                continue
-        else:
-            print("Cannot solve by Newton-Raphson method!\n")
-            return
+        fxi = f(xi)
+        fTagxi = df(xi)
+
+        if (xi < min or xi > max) and xi < 0:
+            xi = x1
+        if xi < min or xi > max:
+            break
+        if fTagxi == 0:
+            print("No solution found.")
+            break
+        if abs(fxi) <= eps:
+            for i in range(len(xiList)):
+                print(f"iteration = {i + 1} xi = {xiList[i]}, f(xi) = {fxList[i]}, f'(xi) = {fTagxList[i]}")
+            print("*" * 50)
+            print(f"Section [{min:.1f} ,{max:.1f}]: Found solution after {iter} iterations: {xi}")
+            print("*" * 50)
+            break
+
+        xiList.append(xi)
+        fxList.append(f(xi))
+        fTagxList.append(df(xi))
+        xi = xi - fxi / fTagxi
+        iter += 1
 
 
-def Secant_Method(polynomial, start_point, end_point, epsilon):
-    check = findSuspects(polynomial, start_point, end_point)
-    roots = []
-    p_prime = polynomial.diff(x)
-    for i in range(len(check)):  # for every suspected section found
-        a = check[i][0]
-        b = check[i][1]
-        roots.append(subSecant(a, b, polynomial, epsilon))
+if __name__ == "__main__":
+    epsilon = 1e-6
+    step = 0.1
+    min_range = -1.1
+    max_range = 2
+    newton_section_list = []
 
-        check = findSuspects(p_prime, start_point, end_point, )
+    # Check if there is a root in a certain value range (in increments of 0.1)
+    for i in np.arange(min_range, max_range, step):
+        i = round(i, 2)
+        if f(i) * f(i + 0.1) <= 0:
+            newton_section_list.append((i, i + step))
 
-        for i in range(len(check)):  # for every suspected section found
-            a = check[i][0]
-            b = check[i][1]
-            tup = subSecant(a, b, polynomial, epsilon)
-            res = polynomial.subs(x, tup[0])
-            if res + epsilon >= 0 and res - epsilon <= 0:
-                roots.append(tup)
-            else:
-                continue
+    # Finding roots by Newton Raphson method:
+    print("-" * 70)
+    print("Roots :")
+    print("-" * 70)
 
-        printRoots(roots)
-
-
-def subSecant(a, b, p, eps):
-    maxIter = int(math.ceil((-1) * (math.log(eps / (b - a)) / math.log(2))))
-    iter = 0
-    i = 1
-    nexti = 2
-    while abs(nexti - i) > eps:
-        if maxIter >= iter:
-            iter += 1
-            fi = p.subs(x, i)
-            fNexti = p.subs(x, nexti)
-            temp = nexti
-            nexti = float(i-(fi*((nexti-i)/(fNexti-fi))))
-            i = temp
-            if abs(fi) <= eps:
-                break
-        else:
-            print("Cannot solve by Secant Method!\n")
-            return
-    return (i, iter)
-
-
-def findSuspects(polynomial, start, end):
-    suspect = []
-    sections = 0.1
-    i = round(start + sections, 2)
-    r1 = polynomial.subs(x, start)
-
-    while i <= end:
-        r2 = polynomial.subs(x, i)
-        if r1 * r2 < 0:
-            suspect.append((round(i - sections, 2), i))
-        if r2 == 0:
-            suspect.append((round(i - sections, 2), i + sections))
-        r1 = r2
-        i = round(i + sections, 2)
-
-    floatList = []
-    for i in range(len(suspect)):
-        floatList.append((float(suspect[i][0]), float(suspect[i][1])))
-    return floatList
-
-def main1():
-    polynomial = sy.exp((math.sin(x ** 4 + 5 * x - 6)) / (2 * math.e ** (-2 * x + 5)))
-    start = -1.5
-    end = 1.5
-    epsilon = 0.0001
-    Bisection_Method(polynomial, start, end, epsilon)
-    Newton_Raphson(polynomial, start, end, epsilon)
-    Secant_Method(polynomial, start, end, epsilon)
-
-main()
-
+    # Sending X0 for which a root approximation is found
+    if len(newton_section_list) == 0:
+        print("cannot use Newton Raphson method")
+    else:
+        for section in newton_section_list:
+            newton(section[0], section[1], epsilon)
